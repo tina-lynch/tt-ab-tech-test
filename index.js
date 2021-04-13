484ec983ced37e7314a2353ade284c68ebbaf8a3
Tina Lynch <tinamarielynch@gmail.com>
2021-04-13 14:17:26 +1000

    refactored api calls and fixed styles

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const axios = require('axios');
 const proxy = `https://cors-anywhere.herokuapp.com/`;
 //visit proxy first to activate access to server
-const baseEndpointProduct = 'https://adorebeauty.com.au/api/ecommerce/catalog/products';
-const baseEndpointCategories = 'https://adorebeauty.com.au/api/ecommerce/catalog/categories';
+const categoriesCalll = 'https://www.adorebeauty.com.au/api/ecommerce/catalog/categories';
+const productsCall = 'https://adorebeauty.com.au/api/ecommerce/catalog/products';
 const tableRows = document.querySelector('.table.t-row');
 const prodTable = document.querySelector('.product-table .t-row');
 const itemDetail = document.querySelector('.item-detail');
@@ -9,42 +10,54 @@ const backBtn = document.querySelector('.back-btn')
 const results = document.querySelector('.results');
 const loader = document.querySelector('.lds-spinner');
 
-
-const getProducts = async () => {
-    const res = await fetch(`${proxy}${baseEndpointProduct}`);
-    const data = await res.json();
-    return data;
-};
-
-const getCategories = async () => {
-    const res = await fetch(`${proxy}${baseEndpointCategories}`);
-    const data = await res.json();
-    return data;
-};
+const getUniqueListBy = (arr, key)=> [...new Map(arr.map(item => [item[key], item])).values()]
+let commonCategories = [];
 
 
-const displayCategory = async () => {
+
+const apiCall = async (url1, url2) => {
+    try {
+        axios.all([
+            axios.get(url1),
+            axios.get(url2)
+        ]).then(axios.spread((categoryData, productData) => {
+
+            productData = productData.data.data.map(item => {
+                let { name, id, sku, description, categories, price } = item;
+                let trimmedProducts = { name, id, sku, description, categories, price };
+                return trimmedProducts
+            })
+
+            categoryData = categoryData.data.data.map(item => {
+                let { name, id } = item;
+                let trimmedCategories = { name, id };
+                return trimmedCategories
+            })
+            
+
+            displayCategory(productData, categoryData);
+            displayProducts(productData);
+            prodTable.addEventListener('click', (e) => showProduct(e, productData, categoryData));
+            
+        }))
+    } catch(err) {
+        console.log(err);
+    }
+   
+}
+
+
+const displayCategory = (productData, categoryData) => {
     loader.classList.remove('hide');
     results.classList.add('hide');
-
-    displayProducts();
     
-    const categoryData = await getCategories();
-    const products = await getProducts();
-    const categoryIds = categoryData.data.map(item => item.id);
-    let productCategoryIds =[];
-    let printCatHeading = [];
-
-    products.data.forEach(item => item.categories.forEach(el => !(categoryIds.includes(el))|| productCategoryIds.push(el)));
-    categoryData.data.forEach(item => !(productCategoryIds.includes(item.id)) || printCatHeading.push(item.name))
-    printCatHeading.map(item=>`${item.name}`);
-    
+    productData.forEach(item => item.categories.forEach(el => categoryData.reduce((acc, x) => !(x.id === el) || commonCategories.push({ id: x.id, name: x.name }), [])) );
+    commonCategories = getUniqueListBy(commonCategories, 'id');
     //not sure if getting the correct categories for the heading
-    //to do: for the heading, check against all categories not just first 50
-    //extra: use cat names for filtering display
-
+    //to do: for the heading, check against all categories not just first 50 // extra: use category names for filtering display
+    
     let h2 = document.createElement('h2');
-    h2.innerHTML = printCatHeading.join(' | ');
+    h2.innerHTML = commonCategories.map(item=>`<a href="#">${item.name}</a>`).join(' | ');
     results.insertAdjacentElement('afterbegin', h2);
 
     loader.classList.add('hide');
@@ -52,53 +65,80 @@ const displayCategory = async () => {
 };
 
 
-const displayProducts = async () => {
-    const products = await getProducts();
-    // const callCategories = await getCategories();
-
-    const html = products.data.map(item =>
-        `<div class="_${item.id}">${item.id} </div>
-        <div class="_${item.id}">${item.name} </div>
-        <div class="_${item.id} right ">${item.sku} </div>`
-    );
-    //to do: pagination
+const displayProducts = (productData) => {
+    const html = productData.map(item =>`
+        <div class="_${item.id}">${item.id} </div>
+        <div class="_${item.id}">${item.name} <br></div>
+        <div class="_${item.id} right ">${item.sku} </div>`);
 
     tableRows.innerHTML = html.join('');
+    loader.classList.add('hide');
+    results.classList.remove('hide');
 }
 
 
-
-const showProduct = async (e) => {
+const showProduct = (e, productData, categoryData) => {
     loader.classList.remove('hide');
     results.classList.add('hide');
-
+    
     let id = e.target;
     id = parseInt(id.classList[0].slice(1));
 
-    let x = await getProducts();
-    x = x.data.filter(item => item.id === id);
+    let openedProduct = productData.filter(item => item.id === id);
+    let catIDs = openedProduct[0].categories;
 
-    const html = x.map(item =>
+/* reworking - start */
+    console.log(openedProduct);
+    console.log(commonCategories)
+    console.log(catIDs)
+    
+    catIDs.forEach(item => commonCategories.map(el => el.id === item ?(el.name) : (item.categories) )       )
+    
+
+    //NOTE TO SELF: checker only shows one category in old code, could try using commonCategories against catIDs, then make checker redundant
+    // maybe break this part out as a seperate function to get correct categories
+    // let checker =[]
+    // commonCategories.filter(item => catIDs.forEach(el=> el === item.id ? checker.push({id: item.id, name: item.name}) : ''));
+
+/* reworking - end */
+
+    const html = openedProduct.map(item =>
         `<div class="data"><h3>${item.name} </h3>
-        <p>Categories ${item.categories}</p>
+        <p>Categories: ${(item.categories).join(', ')}</p>
         <p>SKU: ${item.sku}</p>
-        <p>PRICE: ${item.price}</p>
+        <p>PRICE: $${(item.price).toFixed(2)}</p>
         <p>Description: ${item.description}</p> </div>`
     );
-    //to do: map category list names against ${item.categories} is just showing category numbers for now
 
+
+/* ** old code ** currently displays only category name, not all of them... */
+    // checking categories on the opened product, fallback is category number.. not sure if this is displaying correct info
+    
+    // let checker =[]
+    // categoryData.forEach(cat => x[0].categories.forEach(el=> (el === cat.id) ? checker = [...[cat.id, cat.name]] : ''));
+    
+    // console.log(checker)
+    // const html = x.map(item =>
+    //     `<div class="data"><h3>${item.name} </h3>
+    //     <p>Categories: ${checker[1] ? ((checker[1] + (item.categories.filter(el=> el !== checker[0]).length>0? ', ': ' ')  +  (item.categories.filter(el=> el !== checker[0]).join(', ')))) : (item.categories).join(', ')}</p>
+    //     <p>SKU: ${item.sku}</p>
+    //     <p>PRICE: $${(item.price).toFixed(2)}</p>
+    //     <p>Description: ${item.description}</p> </div>`
+    // );
+/* end  */
+    
     itemDetail.insertAdjacentHTML('beforeend', html.join(''));
     itemDetail.classList.remove('hide');
     loader.classList.add('hide');
 };
 
-const backToList = () =>{
+
+const backToList = () => {
     results.classList.remove('hide');
     itemDetail.classList.add('hide');
     !itemDetail.querySelector('.data') || itemDetail.querySelector('.data').remove()
 };
 
-displayCategory();
 
-prodTable.addEventListener('click', showProduct);
 backBtn.addEventListener('click', backToList);
+apiCall(proxy+categoriesCalll, proxy+productsCall);
